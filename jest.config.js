7f9b8a64ad7d6f2db42142e2f20a08341121ce3d
Emmanuel Cavalcanti <emmanuel.cavalcanti@intelectah.com.br>
2020-07-13 16:46:56 -0300

    Implementação do desafio

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,3 @@
 module.exports = {};
+
+jest.setTimeout(300000);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from "react";
-
-import "./styles.css";
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
+import './styles.css';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then((response) => {
+      const { data } = response;
+      setProjects(data);
+    });
+  });
+
   async function handleAddRepository() {
-    // TODO
+    const repositorio = {
+      title: `Teste ${Date.now()}`,
+      url: 'www.teste.com',
+      techs: [
+        'C#',
+        'asp.net',
+        'MVC',
+        'Typescript',
+        'javascript',
+        'CSS',
+        'HTML',
+        'NodeJS',
+        'React',
+        'Angular',
+      ],
+    };
+
+    const { data } = await api.post('/repositories', repositorio);
+    setProjects([...projects, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    if (id) {
+      await api.delete(`/repositories/${id}`);
+    }
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul data-testid='repository-list'>
+        {projects.map((project) => (
+          <li key={project.id}>
+            {project.title}
+            <button onClick={() => handleRemoveRepository(project.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
